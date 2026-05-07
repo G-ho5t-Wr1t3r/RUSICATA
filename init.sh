1bc9cdc80bb0218163cdc48aa7f99e7394f3021a
G-ho5t-Wr1t3r <murfunig@gmail.com>
2026-05-07 21:01:35 +0200

    modified init for debug and created hot reload

--- init.sh
+++ init.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
-# Parse command line options
-while getopts "i:h" opt; do
+DEBUG=false
+
+while getopts "i:hD" opt; do
   case ${opt} in
     i )
       KEY_PATH=$OPTARG
       ;;
+    D )
+      DEBUG=true
+      ;;
     h )
-      echo "Usage: $0 -i path/of/key"
+      echo "Usage: $0 -i path/of/key [-D]"
+      echo "Options:"
+      echo "  -i    Path to SSH key (required)"
+      echo "  -D    Enable Debug mode (transfers destroy_rusicata.sh & hot_reload.sh)"
+      echo "  -h    Show this help message"
       exit 0
       ;;
     \? )
-      echo "Usage: $0 -i path/of/key"
+      echo "Usage: $0 -i path/of/key [-D]"
       exit 1
       ;;
   esac
 done
 
-# Check key path variable
 if [ -z "$KEY_PATH" ]; then
   echo "Error: Key path required."
-  echo "Usage: $0 -i path/of/key"
+  echo "Usage: $0 -i path/of/key [-D]"
   exit 1
 fi
 
@@ -35,7 +42,14 @@ scp -i "$KEY_PATH" rusicata.tar.gz root@vm:/opt
 echo "Transferring setup via SCP"
 scp -i "$KEY_PATH" setup.sh root@vm:/opt/setup.sh
 
-echo "Transferring destroy_rusicata via SCP"
-scp -i "$KEY_PATH" destroy_rusicata.sh root@vm:/opt/destroy_rusicata.sh
+if [ "$DEBUG" = true ]; then
+  echo "[DEBUG MODE] Transferring destroy_rusicata via SCP"
+  scp -i "$KEY_PATH" destroy_rusicata.sh root@vm:/opt/destroy_rusicata.sh
+
+  echo "[DEBUG MODE] Transferring hot_reload via SCP"
+  scp -i "$KEY_PATH" hot_reload.sh root@vm:/opt/hot_reload.sh
+else
+  echo "Skipping debug scripts (run with -D to transfer destroy_rusicata and hot_reload)"
+fi
 
 echo "Script execution completed"
